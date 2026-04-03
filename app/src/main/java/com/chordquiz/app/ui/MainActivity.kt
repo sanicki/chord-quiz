1b54c0c427a675d6fbf91ec16b9847e1124ef06c
Scott Sanicki <sanicki@github.com>
2026-04-03 17:27:41 -0600

    feat: keep screen awake during quiz sessions

--- app/src/main/java/com/chordquiz/app/ui/MainActivity.kt
+++ app/src/main/java/com/chordquiz/app/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.chordquiz.app.ui
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         enableEdgeToEdge()
         setContent {
             ChordQuizTheme {
