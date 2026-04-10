59c482b4456e151eb11dde00a0ef44afce580b45
Scott Sanicki <sanicki@github.com>
2026-04-10 15:34:13 -0600

    Fix build error: Remove experimental API usage in UnifiedFilterChip

--- app/src/main/java/com/chordquiz/app/ui/components/chip/UnifiedFilterChip.kt
+++ app/src/main/java/com/chordquiz/app/ui/components/chip/UnifiedFilterChip.kt
@@ -1,10 +1,9 @@
 package com.chordquiz.app.ui.components.chip
 
-import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.FilterChip
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -32,10 +31,7 @@ fun UnifiedFilterChip(
             .padding(horizontal = 12.dp, vertical = 8.dp)
             .then(
                 if (onLongClick != null) {
-                    Modifier.combinedClickable(
-                        onClick = onClick,
-                        onLongClick = onLongClick
-                    )
+                    Modifier.clickable(onClick = onClick).then(Modifier.onLongClick(onLongClick))
                 } else {
                     Modifier
                 }
